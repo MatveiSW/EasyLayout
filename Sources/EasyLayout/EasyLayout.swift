308bcad4b824dbe804d393432c588fa969e948c0
Матвей Авдеев <130132470+MatveiSW@users.noreply.github.com>
2024-06-07 12:36:06 +0300

    d

--- Sources/EasyLayout/EasyLayout.swift
+++ Sources/EasyLayout/EasyLayout.swift
@@ -24,8 +24,8 @@ public struct EasyLayout {
         NSLayoutConstraint.activate(
             [
                 view.topAnchor.constraint(equalTo: superview.topAnchor, constant: top * coefHeight),
-                view.leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: -leading * coefWidth),
-                view.trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: -trailing * coefWidth),
+                view.leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: leading * coefWidth),
+                view.trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: trailing * coefWidth),
                 view.bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: bottom * coefHeight)
             ]
         )
