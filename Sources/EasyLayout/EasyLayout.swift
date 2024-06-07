05ba19bf871bff71c86817fb360739970721af23
Матвей Авдеев <130132470+MatveiSW@users.noreply.github.com>
2024-06-07 12:37:28 +0300

    d

--- Sources/EasyLayout/EasyLayout.swift
+++ Sources/EasyLayout/EasyLayout.swift
@@ -25,10 +25,16 @@ public struct EasyLayout {
             [
                 view.topAnchor.constraint(equalTo: superview.topAnchor, constant: top * coefHeight),
                 view.leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: leading * coefWidth),
-                view.trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: trailing * coefWidth),
-                view.bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: bottom * coefHeight)
+                view.trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: -trailing * coefWidth),
+                view.bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: -bottom * coefHeight)
             ]
         )
+    }
+}
+
+extension UIView {
+    func edgeInsets(to superview: UIView, top: CGFloat = 0, leading: CGFloat = 0, trailing: CGFloat = 0, bottom: CGFloat = 0) {
+        EasyLayout.edgeInsets(view: self, to: superview, top: top, leading: leading, trailing: trailing, bottom: bottom)
     }
 }
 
