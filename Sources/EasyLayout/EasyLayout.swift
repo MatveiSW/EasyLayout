1647c14b27b30a756f5beb3c92bfe7c470d51391
Матвей Авдеев <130132470+MatveiSW@users.noreply.github.com>
2024-06-07 12:31:04 +0300

    d

--- Sources/EasyLayout/EasyLayout.swift
+++ Sources/EasyLayout/EasyLayout.swift
@@ -19,24 +19,16 @@ public struct EasyLayout {
     private static let coefHeight = UIScreen.main.bounds.height.adjustSizeHeight()
     private static let coefWidth = UIScreen.main.bounds.width.adjustSizeWidth()
     
-    public static func edgeInsets(view: UIView, to superview: UIView, top: CGFloat?, leading: CGFloat?, trailing: CGFloat?, bottom: CGFloat?) {
+    public static func edgeInsets(view: UIView, to superview: UIView, top: CGFloat, leading: CGFloat, trailing: CGFloat, bottom: CGFloat) {
         view.translatesAutoresizingMaskIntoConstraints = false
-        var constraints = [NSLayoutConstraint]()
-        
-        if let top = top {
-            constraints.append(view.topAnchor.constraint(equalTo: superview.topAnchor, constant: top * coefHeight))
-        }
-        if let leading = leading {
-            constraints.append(view.leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: leading * coefWidth))
-        }
-        if let trailing = trailing {
-            constraints.append(view.trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: -trailing * coefWidth))
-        }
-        if let bottom = bottom {
-            constraints.append(view.bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: -bottom * coefHeight))
-        }
-        
-        NSLayoutConstraint.activate(constraints)
+        NSLayoutConstraint.activate(
+            [
+                view.topAnchor.constraint(equalTo: superview.topAnchor, constant: top * coefHeight),
+                view.leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: leading * coefWidth),
+                view.trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: trailing * coefWidth),
+                view.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: bottom * coefHeight)
+            ]
+        )
     }
 }
 
