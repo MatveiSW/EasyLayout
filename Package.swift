7aab90a94a26785b3974b9289a16bbe484b0d248
Матвей Авдеев <130132470+MatveiSW@users.noreply.github.com>
2024-06-07 09:58:19 +0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "EasyLayout",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "EasyLayout",
+            targets: ["EasyLayout"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "EasyLayout"),
+        .testTarget(
+            name: "EasyLayoutTests",
+            dependencies: ["EasyLayout"]),
+    ]
+)
---
+++ Tests/EasyLayoutTests/EasyLayoutTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import EasyLayout
+
+final class EasyLayoutTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
